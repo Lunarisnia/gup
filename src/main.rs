3b6d98b2ad0a225d48ef2cb9358ae31eb55078e2
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2024-05-02 23:42:22 +0700

    added initialization command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::env::current_dir;
+use std::fs;
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+struct CLI {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    // Init the folder as a gup repository
+    init {}
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = CLI::parse();
+
+    match &cli.command {
+        Some(Commands::init {}) => {
+            match fs::create_dir("./.gup") {
+                Ok(()) => (),
+                Err(e) => println!("{e}")
+            };
+        }
+        None => (),
+    }
 }
