51215d7daf98cd3ff2c500f52cc83b039a65e703
louna <r.arswendo.r@gmail.com>
2024-05-15 17:47:09 +0700

    added compare files function and wip for stage_all

--- src/gup_add.rs
+++ src/gup_add.rs
@@ -1,30 +1,47 @@
 use std::fs;
 use std::path::{Path, PathBuf};
 
-pub fn add(path_buf: &PathBuf) {
-    if !Path::new("./.gup").exists() {
-        return println!("This is not a gup repository");
-    }
-    if path_buf.as_path() == Path::new(".") {
-        let gup_ignore = fs::read_to_string("./.gupignore").unwrap_or_else(|_| "".to_string());
-        let mut ignore_list : Vec<&Path> = Vec::new();
-        ignore_list.push(Path::new("./.gup"));
-        for entry in gup_ignore.split("\n").collect::<Vec<_>>() {
-            ignore_list.push(Path::new(entry));
+pub struct FileStager {
+    // TODO: need a branch manager struct that keep track of current active branch and all available
+}
+
+impl FileStager {
+    pub fn stage(&self, path_buf: &PathBuf) -> Result<(), &str> {
+        if !Path::new("./.gup").exists() {
+            println!("This is not a gup repository");
+            return Err("This is not a gup repository");
+        }
+        match path_buf.as_path().to_str().unwrap() {
+            "." => Self::stage_all(),
+            _ => {}
         }
 
-        match fs::read_dir(".") {
-            Ok(readDir) => {
-                for dir in readDir {
-                    let dir_entry = dir.unwrap().path();
-                    let current_path = dir_entry.as_path();
-                    if !ignore_list.contains(&current_path) {
-                        // TODO: Staging files logic 1 here
-                        println!("NOT GUP: {:?}", current_path)
-                    }
-                }
+        Ok(())
+    }
+
+    fn stage_all() {
+        let gup_ignore = fs::read_to_string("./.gupignore").unwrap_or_else(|_| "".to_string());
+        let mut ignore_list: Vec<PathBuf> = Vec::new();
+        ignore_list.push(Path::new("./.gup").to_path_buf());
+        for entry in gup_ignore.split("\n").collect::<Vec<_>>() {
+            ignore_list.push(Path::new(entry).to_path_buf());
+        }
+
+        let read_dir = fs::read_dir(".").unwrap();
+
+        let mut valid_files: Vec<PathBuf> = Vec::new();
+        for entry_result in read_dir {
+            // It's always this stupid borrow checker error oh my fucking god
+            let entry_path = entry_result.unwrap().path();
+            if !ignore_list.contains(&entry_path) {
+                valid_files.push(entry_path);
             }
-            Err(e) => println!("{}", e)
-        };
+        }
+
+
+        // TODO: Recursor function to walk each file and directory to compare them to the head
+        for file in valid_files {
+            println!("FILE: {:?}", file);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use std::fs::{File, metadata};
+use std::io::{BufRead, BufReader, Read};
+
+use clap::{Error, Parser, Subcommand};
+use crate::gup_add::FileStager;
+
 mod gup_init;
 mod gup_add;
-
-use clap::{Parser, Subcommand};
 
 #[derive(Parser)]
 struct CLI {
@@ -18,14 +22,75 @@ enum Commands {
     add {
         path: std::path::PathBuf,
     },
+
+    compare {
+        file_a: std::path::PathBuf,
+        file_b: std::path::PathBuf,
+    },
 }
 
 fn main() {
     let cli = CLI::parse();
+    let file_stager = FileStager{
+
+    };
 
     match &cli.command {
         Some(Commands::init {}) => gup_init::init_repository("main".to_string()),
-        Some(Commands::add { path }) => gup_add::add(path),
+        Some(Commands::add { path }) => file_stager.stage(path).unwrap(),
+        Some(Commands::compare { file_a, file_b }) => compare_files(file_a, file_b).unwrap(),
         None => (),
     }
+}
+
+fn compare_files(file_a: &std::path::PathBuf, file_b: &std::path::PathBuf) -> Result<(), Error> {
+    let mut identical = false;
+
+    let meta_a = metadata(file_a)?;
+    let meta_b = metadata(file_b)?;
+
+
+    // validate file size
+    if meta_a.len() != meta_b.len() {
+        println!("{:?} != {:?}", file_a, file_b);
+        return Ok(());
+    }
+
+
+    let x = File::open(file_a)?;
+    let y = File::open(file_b)?;
+    let mut x_reader = BufReader::with_capacity(10, x);
+    let mut y_reader = BufReader::with_capacity(10, y);
+
+    loop {
+        let (x_length, y_length) = {
+            let x_buffer = x_reader.fill_buf()?;
+            let y_buffer = y_reader.fill_buf()?;
+
+            if x_buffer != y_buffer {
+               break;
+            }
+
+            (x_buffer.len(), y_buffer.len())
+        };
+
+        if x_length <= 0 && y_length <= 0 {
+            identical = true;
+            break;
+        }
+
+        if x_length <= 0 || y_length <= 0 {
+            break;
+        }
+        x_reader.consume(x_length);
+        y_reader.consume(y_length);
+    }
+
+    if identical {
+        println!("File is identical");
+    } else {
+        println!("Nah Bro");
+    }
+
+    Ok(())
 }
